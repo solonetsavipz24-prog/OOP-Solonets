d944f270377ec17ecc516e9e09cc235131961465
Solonets Artem <solonets.a.v_ipz24@rcitukr.education>
2026-09-11 12:28:07 +0300

    Initial commit

---
+++ lab1v2/Program.cs
@@ -0,0 +1,33 @@
+﻿class Car
+{
+    private string brand;
+    private string model;
+
+    public int Year { get; set; }
+
+    public Car(string brand, string model, int year)
+    {
+        this.brand = brand;
+        this.model = model;
+        Year = year;
+    }
+
+    public void Drive()
+    {
+        Console.WriteLine($"{Year} {brand} {model} їде по дорозі.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car car1 = new Car("BMW", "M5", 2022);
+        Car car2 = new Car("Toyota", "Camry", 2023);
+        Car car3 = new Car("Audi", "A6", 2021);
+
+        car1.Drive();
+        car2.Drive();
+        car3.Drive();
+    }
+}
